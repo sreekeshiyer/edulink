f8e837c5e77b7cc37b4f6642edbc539610e30c78
Sreekesh Iyer <67017933+sreekeshiyer@users.noreply.github.com>
2022-01-31 18:49:53 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,10 @@ export default function Layout(props) {
     return (
         <div>
             <Head>
-                <title>{props.title}</title>
+                    <title>{props.title}</title>
+            <meta charSet="utf-8" />
+            <meta name="description" content="Edulink is a video platform that brings the best learning content from YouTube. It aims at creating an ecosystem for students and budding developers, providing them the best content without involving the YouTube algorithm."></meta>
+            <meta property="og:title" content={props.title} key="ogtitle" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Navbar current={props.current} />
