9a392e6dddfb307a1969783d90f34fb28e6ae180
sreekeshiyer <sreekesh.subramanian@gmail.com>
2022-03-09 17:20:51 +0530

    PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
-module.exports = {
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+module.exports = withPWA({
+    pwa: {
+        dest: "public",
+        register: true,
+        runtimeCaching,
+    },
     images: {
         domains: ["i.ytimg.com", "cdn.vox-cdn.com", "i.gadgets360cdn.com"],
     },
-    webpack: (config) => {
-        config.experiments = { topLevelAwait: true };
-        return config;
-    },
-};
+});
