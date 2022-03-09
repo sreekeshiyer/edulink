5813f2b3d53caf463689f6f92f120b15437eabf2
sreekeshiyer <sreekesh.subramanian@gmail.com>
2022-03-09 13:26:09 +0530

    PWA

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,26 @@ import "@fortawesome/fontawesome-free/css/all.css";
 import { AuthProvider } from "@/context/AuthContext";
 
 function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+        if ("serviceWorker" in navigator) {
+            window.addEventListener("load", function () {
+                navigator.serviceWorker.register("/sw.js").then(
+                    function (registration) {
+                        console.log(
+                            "Service Worker registration successful with scope: ",
+                            registration.scope
+                        );
+                    },
+                    function (err) {
+                        console.log(
+                            "Service Worker registration failed: ",
+                            err
+                        );
+                    }
+                );
+            });
+        }
+    }, []);
     return (
         <AuthProvider>
             <Component {...pageProps} />
