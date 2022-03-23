2b11b79571fd0c25d285a422100907e065b21750
Sreekesh Iyer <67017933+sreekeshiyer@users.noreply.github.com>
2022-03-23 11:13:24 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,6 @@ export default function Layout(props) {
     return (
         <div>
             <Head>
-            <script defer data-domain="edulink.vercel.app" src="https://plausible.io/js/plausible.js"></script>
                     <title>{props.title}</title>
             <meta charSet="utf-8" />
             <meta name="description" content="Edulink is a video platform that brings the best learning content from YouTube. It aims at creating an ecosystem for students and budding developers, providing them the best content without involving the YouTube algorithm."></meta>
