228f3583e7069ae98904b51e2991c173da6f7c90
Sreekesh Iyer <67017933+sreekeshiyer@users.noreply.github.com>
2022-05-04 15:13:22 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,9 +4,8 @@ const runtimeCaching = require("next-pwa/cache");
 module.exports = withPWA({
     pwa: {
         dest: "public",
-        swSrc: "sw.js",
-//         register: true,
-//         runtimeCaching,
+        register: true,
+        runtimeCaching,
     },
     images: {
         domains: ["i.ytimg.com", "cdn.vox-cdn.com", "i.gadgets360cdn.com"],
